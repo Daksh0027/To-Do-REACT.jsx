47bca963c0ddc0e2cc11d402e4ddba11cb3651fd
Daksh0027 <dakshshastri72@gmail.com>
2025-01-03 21:06:13 +0530

    prettier

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function ToDoInput(props) {
-    const { handleAddTodos, todoValue, setTodoValue } = props
-    return (
-        <header>
-        <input value={todoValue} onChange={(e) => {
-            setTodoValue(e.target.value)
-        }} placeholder="Enter a task"/>
-        <button onClick={() => {
-            handleAddTodos(todoValue)
-            setTodoValue('')
-        }}>Add</button>
-        </header>
-    )
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter a task"
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ToDoCard from './ToDoCard'
+import React from "react";
+import ToDoCard from "./ToDoCard";
 
 export default function ToDoList(props) {
-    const {todos} = props
+  const { todos } = props;
 
   return (
-    <ul className='main'>    
-        
-            {todos.map((todo,todoIndex) => {
-                return (
-                    <ToDoCard {...props} key={todoIndex} index = {todoIndex}><p>{todo}</p></ToDoCard>
-                )
-            })}
-        
+    <ul className="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+          <ToDoCard {...props} key={todoIndex} index={todoIndex}>
+            <p>{todo}</p>
+          </ToDoCard>
+        );
+      })}
     </ul>
-    )
+  );
 }
